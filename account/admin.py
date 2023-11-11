c838aef98a69e6088b83d956f3c75c2d2ad737fb
AbhishekJha45 <abhishekjha4123@gmail.com>
2023-11-11 11:31:00 +0530

    backend added

--- account/admin.py
+++ account/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from account.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-class UserModelAdmin(BaseUserAdmin):
+class UserModelAdmin(BaseUserAdmin):  
   # The fields to be used in displaying the User model.
   # These override the definitions on the base UserModelAdmin
   # that reference specific fields on auth.User.
